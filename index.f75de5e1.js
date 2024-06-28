0000f945cfe503141511dead92f6e776b83640ba
Ihor Barban <ihorbarban@Ihors-MacBook-Air.local>
2024-06-28 17:23:26 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve, reject)=>{
+        element.addEventListener(eventName, (e)=>{
+            const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+            resolve(message);
+        });
+    });
+}
+const printMessage = (message)=>{
+    const divMessage = document.createElement("div");
+    divMessage.className = "message";
+    divMessage.textContent = message;
+    document.body.append(divMessage);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
